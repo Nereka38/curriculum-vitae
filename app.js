0f3713715f588adf653c2d93da3eaa84c396a41f
Fernández Cuesta <nerea.fernandez-cuesta@capgemini.com>
2023-01-17 16:47:54 +0100

    add animation and style

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ document.addEventListener("scroll", () => {
     backToTopButton.classList.add("hidden")
   }
 })
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
